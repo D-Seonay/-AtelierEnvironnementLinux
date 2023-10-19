3c051b846919c0e05a3071f2d7ec5fad070b8589
Mathéo DELAUNAY <dseonay@gmail.com>
2023-10-19 12:32:15 +0200

    add branch ScriptBackup

--- scripts/ScriptBackup.sh
+++ scripts/ScriptBackup.sh
@@ -1,1 +1,59 @@
 #!/bin/bash
+
+# Déterminez le rôle de la machine
+machine_role="webserver" # Remplacez par le rôle réel de la machine (par exemple, "database", "webserver", etc.)
+
+machine_roles (){
+  echo "Le rôle de la machine est : $machine_role"
+}
+
+# Répertoire de sauvegarde
+backup_dir="/backup"
+
+# Fonction pour sauvegarder la base de données
+backup_database() {
+    # Code pour sauvegarder la base de données
+    echo "Sauvegarde de la base de données effectuée."
+}
+
+# Fonction pour sauvegarder le site web
+backup_website() {
+    # Code pour sauvegarder le site web
+    echo "Sauvegarde du site web effectuée."
+}
+
+# Fonction pour sauvegarder d'autres éléments en fonction du rôle de la machine
+backup_other_items() {
+    case $machine_role in
+        "webserver")
+            # Code pour sauvegarder d'autres éléments spécifiques au serveur web
+            echo "Sauvegarde d'autres éléments du serveur web effectuée."
+            ;;
+        "database")
+            # Code pour sauvegarder d'autres éléments spécifiques au serveur de base de données
+            echo "Sauvegarde d'autres éléments du serveur de base de données effectuée."
+            ;;
+        *)
+            echo "Rôle de machine inconnu. Aucune sauvegarde effectuée."
+            ;;
+    esac
+}
+
+# Vérifiez le rôle de la machine et effectuez les sauvegardes appropriées
+if [ "$machine_role" == "webserver" ]; then
+    backup_database
+    backup_website
+    backup_other_items
+elif [ "$machine_role" == "database" ]; then
+    backup_database
+    backup_other_items
+else
+    echo "Rôle de machine non défini."
+fi
+
+# Vous pouvez également ajouter des commandes pour archiver les sauvegardes, les stocker localement ou à distance, etc.
+
+# Exemple : archiver les sauvegardes dans un fichier tar
+tar -czf "$backup_dir/backup_$(date +'%Y%m%d%H%M%S').tar.gz" "$backup_dir"
+
+echo "Sauvegardes terminées."
