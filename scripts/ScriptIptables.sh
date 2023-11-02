9dcac65d734027028c9ac4a5804c37df7ab224d6
SashaWilk <sashaw2105@gmail.com>
2023-11-02 15:04:42 +0100

    iptables

--- scripts/ScriptIptables.sh
+++ scripts/ScriptIptables.sh
@@ -1,20 +1,51 @@
 #!/bin/bash
 
-ip_address_web='10.0.2.4'
-ip_address_bdd='10.0.2.15'
+apt install sshpass -y
 
-current_ip=$(hostname -I)
-
-if["$current_ip" = "$ip_address_web"];
-then
-    sudo iptables -A INPUT -p tcp -i eth0 --dport 22 -j ACCEPT
-    sudo iptables -A INPUT -p tcp -i eth0 --dport 80 -j ACCEPT
-    sudo iptables -A INPUT -p tcp -i eth0 --dport 443 -j ACCEPT
-    echo "Machine avec adresse IP correspondant à web. Règles pour serveur Web ajoutées."
-    sudo iptables -P INPUT DROP
-elif["$current_ip" = "$ip_address_bdd"];
-    sudo iptables -A INPUT -p tcp -i eth0 --dport 22 -j ACCEPT
-    sudo iptables -A INPUT -p tcp -i eth0 --dport 3306 -j ACCEPT
-    echo "Machine avec adresse IP correspondant à bdd. Règles pour serveur de base de données ajoutées."
-    sudo iptables -P INPUT DROP
+# Vérification que le fichier CSV est fourni en argument
+if [ $# -ne 1 ]; then
+    echo "Utilisation : $0 fichier.csv"
+    exit 1
 fi
+
+csv_file="$1"
+
+# Vérification de si le fichier existe
+if [ ! -f "$csv_file" ]; then
+    echo "Le fichier $csv_file n'existe pas."
+    exit 1
+fi
+
+# Lecture du fichier CSV et attribution des valeurs aux variables
+while IFS=',' read -r var value; do
+    case "$var" in
+        *)
+            if [ -n "$var" ]; then
+                eval "$var=\"$value\""
+            fi
+            ;;
+    esac
+done < "$csv_file"
+
+# Affichage des valeurs des variables
+echo "web = $web"
+echo "bdd = $bdd"
+
+# Fonction pour exécuter des commandes SSH avec sshpass
+function command_ssh {
+    [ ${#} -gt 0 ] || return 1
+    sshpass -p root ssh -o StrictHostKeyChecking=no kidoly@$1 $2
+}
+
+# Connexion à la VM web
+SUDOPASS="root"  # Remplacez par le mot de passe SSH de votre hôte distant
+
+# Exemple : exécution de commandes SSH
+command_ssh "$web" "echo $SUDOPASS | sudo iptables -A INPUT -p tcp -i eth0 --dport 22 -j ACCEPT"
+command_ssh "$web" "echo $SUDOPASS | sudo iptables -A INPUT -p tcp -i eth0 --dport 80 -j ACCEPT"
+command_ssh "$web" "echo $SUDOPASS | sudo iptables -A INPUT -p tcp -i eth0 --dport 443 -j ACCEPT"
+command_ssh "$web" "echo $SUDOPASS | sudo iptables -P INPUT DROP"
+command_ssh "$bdd" "echo $SUDOPASS | sudo iptables -A INPUT -p tcp -i eth0 --dport 22 -j ACCEPT"
+command_ssh "$bdd" "echo $SUDOPASS | sudo iptables -A INPUT -p tcp -i eth0 --dport 3306 -j ACCEPT"
+command_ssh "$bdd" "echo $SUDOPASS | sudo iptables -P INPUT DROP"
+
